0b4151e253a8e544867f63ab7dcee03bc4a9dc33
kevin <gaoqi198@163.com>
2022-09-18 16:30:12 +0800

    feat: picture concnat

---
+++ opencv/concat.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+def resetRGB(path, t):
+	img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+
+	if len(t) == 0:
+		return img
+
+	h, w, l = img.shape
+	
+	for row in range(h):
+		for col in range(w):
+			b, g, r = img[row, col]
+			if t == "b":
+				img[row, col] = (b, 0, 0)
+			elif t == "g":
+				img[row, col] = (0, g, 0)
+			else:
+				img[row, col] = (0, 0, r)
+	
+	return img
+
+path = r"C:\Users\Administrator\Desktop\\1.jpg"
+
+ori = resetRGB(path, "")
+imgb = resetRGB(path, "b")
+imgg = resetRGB(path, "g")
+imgr = resetRGB(path, "r")
+inputs = np.hstack((ori, imgb, imgg, imgr))
+
+cv2.namedWindow('myPicture', 0)
+cv2.imshow('myPicture',inputs)
+cv2.waitKey(0)
+
