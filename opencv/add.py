13d724bdabbb536be3cc9f53241b90073d67f2e6
高琪 <gaoqi198@163.com>
2023-05-13 12:47:05 +0800

    feat: update

--- opencv/add.py
+++ opencv/add.py
@@ -3,21 +3,28 @@
 import cv2
 import numpy as np
 
-path1 = r"C:\Users\Administrator\Desktop\\1.jpg"
+path1 = r"./images/1.jpg"
 
+# load image
 img = cv2.imread(path1, cv2.IMREAD_UNCHANGED)
+# get height,width
 h, w, l = img.shape
 
-mask = np.ones(img.shape, dtype=np.uint8) * 15 #生成一个和原图同尺寸的单位矩阵，并乘以15
+#生成一个和原图同尺寸的单位矩阵，并乘以15
+mask = np.ones(img.shape, dtype=np.uint8) * 10
 
-imga = img + mask # numpy加法
-imgb = cv2.add(img, mask) #opencv add
+# matrix add,取模处理 (250+10) % 256 = 4
+imga = img + mask
 
-fusion = cv2.add(imga, imgb) #图像融合
-merge = np.hstack((imga, imgb, fusion))
+# opencv add,饱和处理 250 + 10 = 255
+imgb = cv2.add(img, mask) 
+
+# 图像水平拼接
+merge = np.hstack((img, imga, imgb))
 
 hm = cv2.resize(merge, dsize = None, fx = 0.5, fy = 0.5)
 
+# show window
 cv2.imshow('add', hm)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
